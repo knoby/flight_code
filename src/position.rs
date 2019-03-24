a732ef41a34e21ab91e830223994bd4712426b00
knoby <11472032+knoby@users.noreply.github.com>
2019-03-24 19:08:51 +0100

    Added Sensor Unit. Fixed missing file for last commit.

---
+++ src/position.rs
@@ -0,0 +1,18 @@
+use lsm303dlhc;
+
+type I2cSclPin = hal::gpio::PB6<
+    hal::gpio::PullNone,
+    hal::gpio::AltFn<hal::gpio::AF4, hal::gpio::PushPull, hal::gpio::HighSpeed>,
+>;
+
+type I2cSdaPin = hal::gpio::PB7<
+    hal::gpio::PullNone,
+    hal::gpio::AltFn<hal::gpio::AF4, hal::gpio::PushPull, hal::gpio::HighSpeed>,
+>;
+
+type I2c = hal::stm32f30x::I2C1;
+
+pub struct Sensors {
+    //gyro: Type,
+    pub acc: lsm303dlhc::Lsm303dlhc<hal::i2c::I2c<I2c, (I2cSclPin, I2cSdaPin)>>,
+}
