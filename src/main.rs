711998347927be6d404ddea345ec5247b331e35f
knoby <11472032+knoby@users.noreply.github.com>
2019-03-24 10:53:48 +0100

    Changed Deps/Setup/Mem to stm32f303discovery board

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,11 @@
 #![no_std]
 
 // Define Panic behaivior
+// extern crate panic_halt;
 extern crate panic_semihosting;
 
 // Used traits from the HAL crate
-use stm32f1xx_hal::{
-    gpio,
-    prelude::*,
-    serial::{Event, Serial},
-    stm32,
-};
-
-// Asynchronous API
-use nb;
+use alt_stm32f30x_hal::prelude::*;
 
 // Message Passing between Idle, Interrupt and Periodic
 use heapless::{
@@ -27,17 +20,9 @@ mod hardware;
 // Runtime Imports
 use rtfm::{app, Instant};
 
-#[app(device = stm32f1xx_hal::stm32)]
+#[app(device = alt_stm32f30x_hal::stm32f30x)]
 const APP: () = {
     //Resourcen
-
-    /// On Board LED
-    static mut LED: gpio::gpioa::PA5<gpio::Output<gpio::PushPull>> = ();
-
-    /// Transmission Pin for Serial Interface
-    static mut TX: stm32f1xx_hal::serial::Tx<stm32f1xx_hal::stm32::USART2> = ();
-    /// Resive Pin for Serial Interface
-    static mut RX: stm32f1xx_hal::serial::Rx<stm32f1xx_hal::stm32::USART2> = ();
 
     /// Que for passing data from Serial Interrupt to Idle Task
     static mut PSerialRead: Producer<'static, Option<u8>, U4> = ();
@@ -46,9 +31,6 @@ const APP: () = {
     /// Que for passing target positions from Idel Task to Periodic Task
     static mut PTargetPoint: Producer<'static, [f32; 3], U4> = ();
     static mut CTargetPoint: Consumer<'static, [f32; 3], U4> = ();
-
-    // Motor Access
-    static mut Motor: hardware::Motors2x2 = ();
 
     #[init(schedule = [periodic_task])]
     fn init() {
@@ -61,12 +43,11 @@ const APP: () = {
         let _core: rtfm::Peripherals = core;
 
         // Device specific peripherals
-        let device: stm32::Peripherals = device;
+        let device: alt_stm32f30x_hal::stm32f30x::Peripherals = device;
 
         //Freeze clock frequencies
         let mut flash = device.FLASH.constrain();
         let mut rcc = device.RCC.constrain();
-        let mut afio = device.AFIO.constrain(&mut rcc.apb2);
 
         let clocks = rcc
             .cfgr
@@ -74,24 +55,6 @@ const APP: () = {
             .pclk1(32.mhz()) //TODO: Fix bug in hal crate to use different clocks for pclk1/pclk2
             .pclk2(32.mhz())
             .freeze(&mut flash.acr);
-
-        //Configuration of the Pins TX and RX for UART2
-        let mut gpioa = device.GPIOA.split(&mut rcc.apb2);
-        let tx = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
-        let rx = gpioa.pa3.into_floating_input(&mut gpioa.crl);
-
-        let mut serial = Serial::usart2(
-            device.USART2,
-            (tx, rx),
-            &mut afio.mapr,
-            9_600.bps(),
-            clocks,
-            &mut rcc.apb1,
-        );
-
-        //Enable Interrupt for USART2
-        serial.listen(Event::Rxne);
-        let (tx, rx) = serial.split();
 
         //Create que for serial read
         *QSerialRead = Some(Queue::new());
@@ -101,42 +64,21 @@ const APP: () = {
         *QTargetPoint = Some(Queue::new());
         let (pt, ct) = QTargetPoint.as_mut().unwrap().split();
 
-        // Create Pins for the Motor PWM output
-        let mut gpiob = device.GPIOB.split(&mut rcc.apb2);
-        let c1 = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
-        let c2 = gpiob.pb7.into_alternate_push_pull(&mut gpiob.crl);
-        let c3 = gpiob.pb8.into_alternate_push_pull(&mut gpiob.crh);
-        let c4 = gpiob.pb9.into_alternate_push_pull(&mut gpiob.crh);
-
-        let motor_pwm = device.TIM4.pwm(
-            (c1, c2, c3, c4),
-            &mut afio.mapr,
-            1.khz(),
-            clocks,
-            &mut rcc.apb1,
-        );
-
         // Schedule Periodic Task
         schedule
             .periodic_task(Instant::now() + 32_000_000.cycles())
             .unwrap();
 
         //Assign late resources
-        LED = gpioa.pa5.into_push_pull_output(&mut gpioa.crl);
-        TX = tx;
-        RX = rx;
-
         PSerialRead = ps;
         CSerialRead = cs;
 
         PTargetPoint = pt;
         CTargetPoint = ct;
-
-        Motor = hardware::Motors2x2 { motor_pwm };
     }
 
     /// Idle Task for non critical jobs
-    #[idle(resources = [CSerialRead, PTargetPoint, TX])]
+    #[idle(resources = [CSerialRead, PTargetPoint])]
     fn idle() -> ! {
         // Buffer for message evaluation
         let mut in_buffer = [0_u8; 32];
@@ -144,43 +86,13 @@ const APP: () = {
         let mut msg = [0_u8; 32];
 
         loop {
-            if let Some(item) = resources.CSerialRead.dequeue() {
-                // Item in Que
-                match item {
-                    Some(rc_framing::framing::END) => {
-                        if let Ok(_msg_len) =
-                            rc_framing::framing::decode(&in_buffer[..rec_len], &mut msg)
-                        {
-                            nb::block!(resources.TX.write(b"O"[0])).unwrap();
-                            nb::block!(resources.TX.write(b"k"[0])).unwrap();
-                            nb::block!(resources.TX.write(15)).unwrap();
-                        } else if rec_len > 0 {
-                            nb::block!(resources.TX.write(b"E"[0])).unwrap();
-                            nb::block!(resources.TX.write(b"r"[0])).unwrap();
-                            nb::block!(resources.TX.write(b"r"[0])).unwrap();
-                            nb::block!(resources.TX.write(15)).unwrap();
-                        };
-                        rec_len = 0;
-                    }
-                    Some(byte) => {
-                        in_buffer[rec_len] = byte;
-                        rec_len += 1;
-                    }
-                    None => {
-                        //overflow in input,
-                        rec_len = 0;
-                    }
-                }
-            }
+            cortex_m::asm::nop();
         }
     }
 
     /// Periodic task for real time critical things
-    #[task(priority = 5 , resources = [CTargetPoint, LED], schedule = [periodic_task])]
+    #[task(priority = 5 , resources = [CTargetPoint], schedule = [periodic_task])]
     fn periodic_task() {
-        //Toggle LED
-        resources.LED.toggle();
-
         //Rescedule task
         schedule
             .periodic_task(scheduled + 32_000_000.cycles())
@@ -188,15 +100,8 @@ const APP: () = {
     }
 
     /// Interrupt for reading data from serial interface
-    #[interrupt(priority = 10, resources = [RX, PSerialRead])]
-    fn USART2() {
-        //Read the recived Byte from the interface and push it in the queue
-        if let Ok(byte) = resources.RX.read() {
-            resources.PSerialRead.enqueue(Some(byte)).is_err();
-        } else {
-            resources.PSerialRead.enqueue(None).is_err();
-        }
-    }
+    #[interrupt(priority = 10, resources = [ PSerialRead])]
+    fn USART2_EXTI26() {}
 
     // Interrupt handlers used to dispatch software tasks
     extern "C" {
